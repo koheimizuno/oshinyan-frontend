4c90243cad9f73529e041cecfa98594ff2a43b24
michioyamada <skygentleman626@gmail.com>
2023-12-27 10:19:55 +0900

    updated top page

--- src/components/common/Store.tsx
+++ src/components/common/Store.tsx
@@ -26,22 +26,20 @@ const items = [
 const Store = () => {
   return (
     <div className="bg-white">
-      <div className="max-w-[960px] xs:px-5 lg:px-0 m-auto">
-        <div className="py-[48px]">
-          <div className="text-center pb-[48px]">
-            <h3 className=" w-full border-y border-solid border-[#707070] text-center text-[32px] inline-block">
-              推しニャン！コラム
-            </h3>
-          </div>
-          <div className="flex justify-between flex-wrap">
-            <div className="m-auto">
-              {items.map((e, i) => (
-                <Guess key={i} imgUrl={e.imgUrl} />
-              ))}
-            </div>
-          </div>
-          <MoreButton />
+      <div className="max-w-[960px] xs:px-5 lg:px-0 m-auto py-[48px]">
+        <div className="text-center pb-[48px]">
+          <h3 className=" w-full border-y border-solid border-[#707070] text-center text-[32px] inline-block">
+            推しニャン！コラム
+          </h3>
         </div>
+        <div className="m-auto">
+          <div className="flex justify-between flex-wrap">
+            {items.map((e, i) => (
+              <Guess key={i} imgUrl={e.imgUrl} />
+            ))}
+          </div>
+        </div>
+        <MoreButton />
       </div>
     </div>
   );
